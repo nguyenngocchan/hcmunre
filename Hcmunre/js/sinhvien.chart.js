7f07b6c3524e6e559f217df0ecc53b111bb7666d
sp installer <spinstaller@ngocchan.com>
2017-11-11 16:40:53 +0700

    up

---
+++ Hcmunre/js/sinhvien.chart.js
@@ -0,0 +1,113 @@
+﻿function selectValue() {
+d = document.getElementById("select_id").value;
+return d;}
+function getLop() {
+    // Getting our list items
+    $.ajax({
+        url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getbytitle('Sinhvien')/items?$select=Lop",
+        method: "GET",
+        headers: { "Accept": "application/json; odata=verbose" },
+        success: function (data) {
+            var items = [];
+            
+            for (var i in data.d.results) {
+                var lop = (data.d.results[i].Lop)?data.d.results[i].Lop:'';
+                var arrlop={LOP:lop};
+                var existed=false;
+                $.each(items,function(idx,val){
+                        var currentItem=items[idx];
+                        if(currentItem.LOP===lop){
+                            existed=true;
+                        }               
+                    }
+                );  
+                if(!existed){
+                    items.push(arrlop);
+                }
+                for(var idx = 0; idx < items.length; idx++) {
+                    var s=items[idx].LOP;
+                    var html=('<option value="'+s+'">'+s+'</option>')
+                    items.push(html);
+                }
+                jQuery('#select_id').append(items.join(''));
+            }
+        },
+        error: function (data) {
+            
+        }
+        });
+    }
+
+
+
+
+
+
+
+$(document).ready(function () {
+    var listName = "Duyệt công tác";
+    /*https://social.technet.microsoft.com/wiki/contents/articles/35796.sharepoint-2013-using-rest-api-for-selecting-filtering-sorting-and-pagination-in-sharepoint-list.aspx*/
+	var query = "?$select=Tr_x1ea1_ng_x0020_th_x00e1_i";
+      
+ var t5spjq = new $.t5_sp_jq({ url: _spPageContextInfo.webServerRelativeUrl });
+    var arrChart = [];
+    var arrFn = [t5spjq.getValuesOfChoice(listName, 'Tr_x1ea1_ng_x0020_th_x00e1_i'),
+        t5spjq.getListItems(listName, query)
+    ];
+    $.when.apply($, arrFn).then(function () {
+        var allData = arguments;
+        var values = allData[0].d.results[0].Choices.results;
+
+        var itms = allData[1].d.results;
+        if (itms.length > 0) {
+            $.each(values, function (index, value) {
+                arrChart.push({ 'name': value, 'y': 0 });
+            });
+            $.each(itms, function (idx, itm) {
+                var n = arrChart.length;
+                for (var i = 0; i < n; i++) {
+                    if (arrChart[i].name == itm.Tr_x1ea1_ng_x0020_th_x00e1_i) {
+                        arrChart[i].y = arrChart[i].y + 1;
+                        break;
+                    }
+                }
+            });
+            bar_char(arrChart);
+        } else
+            bar_char([]);
+    }, function (mess) {
+        console.log(mess);
+
+    });
+	function bar_char(data) {
+	Highcharts.chart('container', {
+            chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Thống kê tình trạng xử lý các yêu cầu duyệt công tác'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Brands',
+            colorByPoint: true,
+            data: data
+        }]
+        });
+}
+});
---
+++ Hcmunre/js/spJQRest.js
@@ -0,0 +1,184 @@
+﻿(function ($) {
+    /*https://www.plusconsulting.com/blog/2013/05/crud-on-list-items-using-rest-services-jquery/*/
+    /* Getting the item type for the list */
+    function getListItemType(name) {
+        return "SP.Data." + name[0].toUpperCase() + name.substring(1) + "ListItem";
+    }
+    $.t5_sp_jq = function (option) {
+        $.t5_sp_jq.settings = $.extend({
+            url: _spPageContextInfo.webAbsoluteUrl,
+            REQUESTDIGEST: $("#__REQUESTDIGEST").val(),
+        }, option);
+    }
+    
+    $.t5_sp_jq.prototype = {
+    	getCurrentUser: function() {    
+			var dfd = new $.Deferred();
+		    $.ajax({
+		        url: $.t5_sp_jq.settings.url + "/_api/web/currentUser?$expand=groups",
+		       	headers: { "Accept": "application/json; odata=verbose" },
+		        success: function (data) {
+		            dfd.resolve(data);
+		        },
+		        error: function (data) {
+		            dfd.reject(data);
+		        }
+		    });
+		    return dfd.promise();
+		},
+		
+		 
+   
+		getValuesOfChoice: function (listname, fieldName) {
+            var dfd = new $.Deferred();
+            $.ajax({
+                url: $.t5_sp_jq.settings.url + "/_api/web/lists/getbytitle('" + listname + "')/fields?$filter=EntityPropertyName eq '" + fieldName + "'",
+                method: "GET",
+                headers: { "Accept": "application/json; odata=verbose" },
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (data) {
+                    dfd.reject(data);
+                }
+            });
+            return dfd.promise();
+        },
+        
+        
+        getTotalItems: function (listname, filter) {
+            var dfd = new $.Deferred();            
+            var url = ''; 
+            //?$filter=SubjectsID eq' + itemid
+            if(filter === undefined && filter == null){
+            	url = $.t5_sp_jq.settings.url + "/_api/web/lists/getbytitle('" + listname + "')/itemcount";
+            }else{
+            	url = $.t5_sp_jq.settings.url + "/_api/web/lists/getbytitle('" + listname + "')/itemcount"+ filter;
+            }
+            $.ajax({
+                url: url ,
+                method: "GET",
+                headers: { "Accept": "application/json; odata=verbose" },
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (data) {
+                    dfd.reject(data);
+                }
+            });
+            return dfd.promise();
+        },
+        /*Get a Single List Item*/
+        getListItem: function (listname, id) {
+            var dfd = new $.Deferred();
+            $.ajax({
+                url: $.t5_sp_jq.settings.url + "/_api/web/lists/getbytitle('" + listname + "')/items(" + id + ")",
+                method: "GET",
+                headers: { "Accept": "application/json; odata=verbose" },
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (data) {
+                    dfd.reject(data);
+                }
+            });
+            return dfd.promise();
+        },
+        /*Getting list items based on ODATA Query*/
+        getListItems: function(listname, query) {
+            var dfd = new $.Deferred();
+            $.ajax({
+                url: $.t5_sp_jq.settings.url + "/_api/web/lists/getbytitle('" + listname + "')/items" + query,
+                method: "GET",
+                headers: { "Accept": "application/json; odata=verbose" },
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (data) {
+                    dfd.reject(data);
+                }
+            });
+            return dfd.promise();
+        },
+        // Adding a list item with the metadata provided
+        addListItem: function(url, listname, metadata) {
+            var dfd = new $.Deferred();
+            var item = $.extend({
+                "__metadata": { "type": getListItemType(listname)}
+            }, metadata);
+
+            $.ajax({
+                url: $.t5_sp_jq.settings.url + "/_api/web/lists/getbytitle('" + listname + "')/items",
+                type: "POST",
+                contentType: "application/json;odata=verbose",
+                data: JSON.stringify(item),
+                headers: {
+                    "Accept": "application/json;odata=verbose",
+                    "X-RequestDigest": $.t5_sp_jq.settings.REQUESTDIGEST
+                },
+                success: function (data) {
+                    dfd.resolve(data);
+                },
+                error: function (data) {
+                    dfd.reject(data);
+                }
+            });
+            return dfd.promise();
+        },
+        updateListItem: function (listname, id, metadata) {
+            var obj = this;
+            var dfd = new $.Deferred();
+            var item = $.extend({
+                "__metadata": { "type": getListItemType(listname) }
+            }, metadata);
+
+            obj.getListItem(listname, id).then(function (data) {
+                $.ajax({
+                    url: data.d.__metadata.uri,
+                    type: "POST",
+                    contentType: "application/json;odata=verbose",
+                    data: JSON.stringify(item),
+                    headers: {
+                        "Accept": "application/json;odata=verbose",
+                        "X-RequestDigest": $.t5_sp_jq.settings.REQUESTDIGEST,
+                        "X-HTTP-Method": "MERGE",
+                        "If-Match": data.d.__metadata.etag
+                    },
+                    success: function (data) {
+                        dfd.resolve(data);
+                    },
+                    error: function (data) {
+                        dfd.reject(data);
+                    }
+                });
+            }, function (data) {
+                dfd.reject(data);
+            });
+            return dfd.promise();
+        },
+        // Deleting a List Item based on the ID
+        deleteListItem: function (listname, id, success, failure) {
+            var obj = this;
+            var dfd = new $.Deferred();
+            obj.getListItem(listname, id).then(function (data) {
+                $.ajax({
+                    url: data.d.__metadata.uri,
+                    type: "POST",
+                    headers: {
+                        "Accept": "application/json;odata=verbose",
+                        "X-Http-Method": "DELETE",
+                        "X-RequestDigest": $.t5_sp_jq.settings.REQUESTDIGEST,
+                        "If-Match": data.d.__metadata.etag
+                    },
+                    success: function (data) {
+                        dfd.resolve(data);
+                    },
+                    error: function (data) {
+                        dfd.reject(data);
+                    }
+                });
+            });
+            return dfd.promise();
+        },
+    }
+}(jQuery));
