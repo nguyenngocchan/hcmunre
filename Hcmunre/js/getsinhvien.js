86d4ebf8fae4b9089e280bfbb837ecc5224ac52c
sp installer <spinstaller@ngocchan.com>
2017-11-14 14:10:45 +0700

    a

--- Hcmunre/js/getsinhvien.js
+++ Hcmunre/js/getsinhvien.js
@@ -1,109 +1,124 @@
-var myAngApp = angular.module('SharePointAngApp', []);  
-myAngApp.controller('spsinhvienController',  
-    function($scope, $http)  
-    {$http(  
-        {  
-            method: 'GET',  
-            url: _spPageContextInfo.webAbsoluteUrl +"/_api/web/lists/getByTitle('Sinhvien')/items?$select=Id,Title,Masinhvien,Lop,Tinhtrang",  
-            headers:  
-            {  
-                "Accept": "application/json;odata=verbose"  
-            }  
-        }).success(function(data, status,  
-            headers, config)  
-        {  
-			$scope.sinhvien = data.d.results; 
-			$scope.totalItems = $scope.sinhvien.length;  
-			$scope.numPerPage = 5;
-			$scope.currentPage = 1; 
-			$scope.paginate = function (value) {  
-				var begin, end, index;  
-	            begin = ($scope.currentPage - 1) * $scope.numPerPage;  
-	            end = begin + $scope.numPerPage;  
-	            index = $scope.customers.indexOf(value);  
-	            return (begin <= index && index < end);  
-	        };
-	        $scope.getByDataID = function (id) { 
-			var sinhvienid= id;
-			$.ajax({
-			      url: _spPageContextInfo.webAbsoluteUrl +"/_api/web/lists/getByTitle('Sinhvien')/items?$select=Id,Title,Masinhvien,Lop,Tinhtrang",  			      type: "GET",
-			      headers: 
-			      {
-			            "Accept": "application/json;odata=verbose",
-			            "content-type": "application/json; odata=verbose",
-			            "X-RequestDigest": $("#__REQUESTDIGEST").val()
-			                    
-			       },
-				success: function(data){
-					$scope.sinhvien=data.d; 
-					
-				},
-				error: function(err){
-					alert("Error while fetching list item: " + JSON.stringify(err));
-				}
-			
-			  });
-			};
-            $scope.Delete= function (id) { 
-       		var idSinhvien=id;
-       		$.ajax({
-              url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('Sinhvien')/items('"+idSinhvien+"')",
-              type: "POST",
-              headers: {
-                               "Accept": "application/json;odata=verbose",
-                               "content-type": "application/json; odata=verbose",
-                               "X-RequestDigest": $("#__REQUESTDIGEST").val(),
-                               "X-HTTP-Method": "DELETE",
-                               "If-Match": "*"
-                            },
-              success: function(data){
-                  alert("Item deleted successfully!");
-                  location.reload();
+var myAngApp = angular.module('SharePointAngApp', []);
+myAngApp.controller('spSinhvienController', function($scope, $http) {
+    $scope.eEditable = -1;
+    $http({
+        method: 'GET',
+        url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/getByTitle('Sinhvien')/items?$select=ID,Title,Masinhvien,Lop,Tinhtrang",
+        headers: {
+            "Accept": "application/json;odata=verbose"
+        }
+    }).success(function(data, status, headers, config) {
+        $scope.sinhviens = data.d.results;
+        // var a= $scope.customers;
+    }).error(function(data, status, headers, config) {});
+    //$scope.customers = [];
+    $scope.getByDataID = function(sinhvien) {
+        var sinhvienId = sinhvien;
+        $.ajax({
+            url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('Sinhvien')/getitembyid(" + sinhvienId + ")/?$select=ID,Title,Masinhvien,Lop,Tinhtrang",
+            type: "GET",
+            headers: {
+                "Accept": "application/json;odata=verbose",
+                "content-type": "application/json; odata=verbose",
+                "X-RequestDigest": $("#__REQUESTDIGEST").val()
+            },
+            success: function(data) {
+                $("#btnupdateval").prop("disabled", false);
+                $scope.sinhvien = data.d;
+                $('#txtItemId').val(data.d.Id);
+                $('#txttitle').val(data.d.Title);
+                $('#txtma').val(data.d.Masinhvien);
+                $('#txtlop').val(data.d.Lop);
+                $('#txttinhtrang').val(data.d.Tinhtrang);
+                $("#btnsave").prop("disabled", true);
+            },
+            error: function(err) {
+                alert("Error while fetching list item: " + JSON.stringify(err));
+            }
+        });
+    };
+    $scope.Save = function() {
+        $.ajax({
+            url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('Sinhvien')/items",
+            type: "POST",
+            data: JSON.stringify({
+                '__metadata': {
+                    'type': 'SP.Data.UserTestListItem'
                 },
-              error: function(err){
-                                  console.log(JSON.stringify(error));                            }
-
-		          });
-		    }; 
-		    $scope.Update = function (id) {
-		    	var idSinhvien=id;
-				$.ajax({
-				url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('Sinhvien')/items("+idSinhvien+")",
-				type: "POST",
-				data: JSON.stringify({
-				       
-				       '__metadata': {'type': 'SP.Data.SinhvienListItem' },
-				          "Title": document.getElementById('txthoten').value,
-				 		"Masinhvien":document.getElementById('txtma').value,
-				 		"Lop":document.getElementById('txtlop').value,
-				 		"Tinhtrang":document.getElementById('txttingtrang').value
-				          }),
-				headers: {
-				               "Accept": "application/json;odata=verbose",
-				               "content-type": "application/json; odata=verbose",
-				               "X-RequestDigest": $("#__REQUESTDIGEST").val(),
-				               "X-HTTP-Method": "MERGE",
-				               "If-Match": "*"
-				            },
-				success: function(data){
-	              alert("Item updated successfully!");
-	            	},
-				error: function(err){
-                	alert("Error while updating item: " + JSON.stringify(err));
-	            	}
-				});
-			
-			};  
-			$scope.mySortFunction = function(  
-                sinhvien)  
-            { //Sorting Iteam  
-                if (isNaN(sinhvien[$scope.sortExpression]))  
-                    return sinhvien[$scope.sortExpression];  
-                return parseInt(sinhvien[$scope  
-                    .sortExpression]);  
-            }  
-        }).error(function(data, status,  
-            headers, config) {  
-        });  
-    });    
-  
+                "Title": $scope.sinhvien.Title,
+                "Masinhvien": $scope.sinhvien.Masinhvien,
+                "Lop": $scope.sinhvien.Lop
+            }),
+            headers: {
+                "Accept": "application/json;odata=verbose",
+                "content-type": "application/json; odata=verbose",
+                "X-RequestDigest": $("#__REQUESTDIGEST").val()
+            },
+            success: function(data) {
+                alert("Item added successfully!");
+            },
+            error: function(err) {
+                alert("Error while adding item: " + JSON.stringify(err));
+            }
+        });
+    };
+    $scope.Update = function() {
+        $.ajax({
+            url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('Sinhvien')/items(" + $scope.sinhvien.ID + ")",
+            type: "POST",
+            data: JSON.stringify({
+                '__metadata': {
+                    'type': 'SP.Data.SinhvienListItem'
+                },
+                "Title": $scope.sinhvien.Title,
+                "Masinhvien": $scope.sinhvien.Masinhvien,
+                "Lop": $scope.sinhvien.Lop
+            }),
+            headers: {
+                "Accept": "application/json;odata=verbose",
+                "content-type": "application/json; odata=verbose",
+                "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+                "X-HTTP-Method": "MERGE",
+                "If-Match": "*"
+            },
+            success: function(data) {
+                alert("Item updated successfully!");
+            },
+            error: function(err) {
+                alert("Error while updating item: " + JSON.stringify(err));
+            }
+        });
+    };
+    $scope.Delete = function(sinhvien) {
+        var sinhvienId = sinhvien;
+        $.ajax({
+            url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('Sinhvien')/items(" + sinhvienId + ")",
+            type: "POST",
+            headers: {
+                "Accept": "application/json;odata=verbose",
+                "content-type": "application/json; odata=verbose",
+                "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+                "X-HTTP-Method": "DELETE",
+                "If-Match": "*"
+            },
+            success: function(data) {
+                alert("Item deleted successfully!");
+            },
+            error: function(err) {
+                alert("Error while deleting item: " + JSON.stringify(err));
+            }
+        });
+    };
+    $scope.ClearFields = function() {
+        $("#btnsave").prop("disabled", false);
+        $scope.customer = "";
+    };
+    // Sorting For 
+	$scope.mySortFunction = function(sinhvien)  
+	{ //Sorting Iteam  
+		if (isNaN(sinhvien[$scope.sortExpression]))  
+		    return sinhvien[$scope.sortExpression];  
+		return parseInt(sinhvien[$scope  
+		    .sortExpression]);  
+	}  
+});
