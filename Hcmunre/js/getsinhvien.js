b81e11c11f33edbaf75a880f26761fe294276c1d
sp installer <spinstaller@ngocchan.com>
2017-12-02 00:31:02 +0700

    a

--- Hcmunre/js/getsinhvien.js
+++ Hcmunre/js/getsinhvien.js
@@ -46,7 +46,7 @@ myAngApp.controller('spSinhvienController', function($scope, $http) {
         });
     };
     $scope.Save = function() {
-        $("#spadd").show();
+      $("#spadd").show();
         $("#btnadd").attr("disabled", "disabled");
         $.ajax({
             url: _spPageContextInfo.webAbsoluteUrl + "/_api/web/lists/GetByTitle('Sinhvien')/items",
@@ -259,7 +259,19 @@ function getLookupLop(){
 
     });
 }
+var tableToExcel = (function() {
+          var uri = 'data:application/vnd.ms-excel;base64,'
+            , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+            , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+            , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+          return function(table, name) {
+            if (!table.nodeType) table = document.getElementById(table)
+            var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+            window.location.href = uri + base64(format(template, ctx))
+          }
+        })()
+
 function showNew(){
-    $('#btnadd').show();
-    $('#btnupdateval').hide();
+  $('#btnadd').show();
+  $('#btnupdateval').hide();
 }
